ad3aa251d4cf8253b0461037907bf004fe3cb60a
lawrencemboya <lawrencemboya@gmail.com>
2020-06-24 23:23:10 -0400

    created, whenGivenAnArrayOfThreeIndexes_ShouldReturnTwoIndexesThatSumToTarget()

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-    public int[] calculate(int[] givenArray, int target) {
-        return new int[] {0, 1};
+
+    public int[] solution(int[] givenNums, int target) {
+//        return new int[] {0, 1};
+
+        int[] results = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < givenNums.length ; i++) {
+            if(map.containsKey(target - givenNums[i])){
+                results[1] = i;
+                results[0] = map.get(target - givenNums[i]);
+            }
+            map.put(givenNums[i], i);
+        }
+        return results;
     }
 }
--- src/test/java/TwoSumTest.java
+++ src/test/java/TwoSumTest.java
@@ -4,19 +4,34 @@ import org.junit.Test;
 public class TwoSumTest {
 
     @Test
-    public void whenGivenAnArrayOfTwoIndexes_ShouldReturnTheIndexes() {
+    public void whenGivenAnArrayOfTwoIndexes_ShouldReturnTwoIndexesThatSumsToTarget() {
         //Arrange
         TwoSum twoSum = new TwoSum();
 
-        int target = 5;
-        int[] givenArray = new int[]{2, 3};
+        int target = 9;
+        int[] givenNums = new int[]{2, 7};
         int[] returnedArrayOrExpected = new int[]{0, 1};
 
         //Act
-        int[] actual = twoSum.calculate(givenArray, target);
+        int[] actual = twoSum.solution(givenNums, target);
 
         //Assertion
         Assert.assertArrayEquals(returnedArrayOrExpected, actual);
     }
 
+    @Test
+    public void whenGivenAnArrayOfThreeIndexes_ShouldReturnTwoIndexesThatSumToTarget() {
+        //Arrange
+        TwoSum twoSum = new TwoSum();
+
+        int target = 13;
+        int[] givenArray = new int[]{2, 7, 11};
+        int[] expectedOrReturnedArray = new int[]{0, 2};
+
+        //Act
+        int[] actual = twoSum.solution(givenArray, target);
+
+        //Assertion
+        Assert.assertArrayEquals(expectedOrReturnedArray, actual);
+    }
 }
